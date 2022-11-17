9b623dc6e6b94aa64a13aab0af55da4435926498
Prisha Maiti <89211997+PrishaMaiti@users.noreply.github.com>
2022-11-16 17:29:16 -0800

    css changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,15 @@ import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div className='landing-page'>
-      <Flex direction='column' padding={5} mx='10vw' bg='#3D165E'>
-        <Text ml='25vw' mb='10vh' fontSize={70} className='title'><b>Welcome to MSS</b></Text>
-        <Flex>
-          <Box w='10vw' h='15vh' bg='red.500'>
-            <Text>1000 students</Text>
-          </Box>
+    <div className='landing-page' width='100vw'>
+      <Flex direction='column' >
+        <Text height='10vh' mx='10' align='center' fontSize='7xl' className='title' ><b>Welcome to MSS</b></Text>
+        <Flex height='86vh' mx='20' alignItems='center'>
+          <Box className='landing-page-btnbox'>1000 students</Box>
           <Spacer />
-          <Box w='10vw' h='15vh' bg='red.500'>
-            <Text>100 teachers</Text>
-          </Box>
+          <Box className='landing-page-btnbox'>200 teachers</Box>
           <Spacer />
-          <Box w='10vw' h='15vh' bg='red.500'>
-            <Text>20 schools</Text>
-          </Box>
+          <Box className='landing-page-btnbox'>60 schools</Box>
         </Flex>
       </Flex>
     </div>
